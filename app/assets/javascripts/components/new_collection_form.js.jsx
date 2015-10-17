73553de6b6fc4891f2dad741e61f23a8ddfcff10
Nathan Specht <nathanspecht@gmail.com>
2015-10-16 17:33:43 -0700

    Styling

--- app/assets/javascripts/components/new_collection_form.js.jsx
+++ app/assets/javascripts/components/new_collection_form.js.jsx
@@ -10,17 +10,33 @@ NewCollectionForm = React.createClass({
   _addCollection: function() {
     ApiUtil.saveCollection(this.state);
     this.props._showButton();
+    this.setState({title: ""});
+  },
+
+  _handleKeyDown: function(e) {
+    if (e.key === "Escape") {
+      this.props._showButton();
+      this.setState({title: ""});
+    }
   },
 
   render: function() {
     return (
-      <form>
-        <label>
-          <input type="text" valueLink={this.linkState('title')} />
-        </label>
-        <button className="button submit-button"
-                onClick={this._addCollection}>Create</button>
-      </form>
+      <div>
+        <form>
+          <label>
+            <input type="text"
+                   valueLink={this.linkState('title')}
+                   onKeyDown={this._handleKeyDown}
+                   autoFocus/>
+          </label>
+          <button className="hidden"
+                  onClick={this._addCollection}>Create</button>
+        </form>
+        <div className="tagline">
+          Press enter to add or escape to cancel.
+        </div>
+      </div>
     );
   }
 });
